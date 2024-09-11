e317a092cb47d747486c6f3721c6bb6ca933cb19
kero harby <111562259+kyrolloss@users.noreply.github.com>
2024-09-11 15:18:47 +0300

    handel Settings UI

---
+++ lib/Features/MainLayout/Presentation/View/Settings View/ My Profile Tap/My Profile Tap.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class  MyProfileTap extends StatelessWidget {
+  const  MyProfileTap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
